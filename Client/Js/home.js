a0d82c19fd8bc10ffadb19c31fbad4c63665deb2
Soltan <mahmudovsoltan477@gmail.com>
2025-01-14 18:54:05 +0400

    add home carusel

---
+++ Client/Js/home.js
@@ -0,0 +1,35 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+
+  var swiper2 = new Swiper(".mySwiper2", {
+    watchSlidesProgress: true,
+    slidesPerView: 5,
+    spaceBetween: 16,
+    // loop: true,
+  });
+
+  var swiper3 = new Swiper(".mySwiper3", {
+    watchSlidesProgress: true,
+    slidesPerView: 3,
+    spaceBetween: 16,
+    // loop: true,
+  });
+
+
