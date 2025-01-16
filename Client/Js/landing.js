24ffc4beb0d4b725884f5c249d6dfeb8ebc4edc8
NarminJalilova <adilovanarmin95@gmail.com>
2025-01-16 17:45:52 +0400

    Completed

--- Client/Js/landing.js
+++ Client/Js/landing.js
@@ -1,9 +1,9 @@
 // FAQ Toggle Logic
-const faqItems = document.querySelectorAll('.faq-item');
+const faqItems = document.querySelectorAll('.faq_item');
 
 faqItems.forEach(item => {
     const question = item.querySelector('h3');
-    const content = item.querySelector('.faq-content');
+    const content = item.querySelector('.faq_content');
     question.addEventListener('click', () => {
         const isVisible = content.style.display === 'block';
         content.style.display = isVisible ? 'none' : 'block';
