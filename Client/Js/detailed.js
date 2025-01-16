b5f8f9084a45105792c6a61fed51203c24adc122
RamilShahmerdanov <shahmerdanovr20@gmail.com>
2025-01-16 14:40:26 +0400

    update detailed page

--- Client/Js/detailed.js
+++ Client/Js/detailed.js
@@ -1,13 +1,45 @@
-function addComment() {
-  const commentInput = document.querySelector("#commentInput");
-  const addedcomment = document.querySelector("#addedcomment");
-  data = commentInput.value.jSON;
+// function addComment() {
+//   const commentInput = document.querySelector("#commentInput");
+//   const addedcomment = document.querySelector("#addedcomment");
+//   data = commentInput.value.jSON;
 
-  if (commentInput.value.trim() !== "") {
-    addedcomment.innerText += commentInput.value;
-    commentInput.value = "";
-    console.log(commentInput.value);
-  } else {
-    console.log("serh yazin");
-  }
-}
+//   if (commentInput.value.trim() !== "") {
+//     addedcomment.innerText += commentInput.value;
+//     commentInput.value = "";
+//     console.log(commentInput.value);
+//   } else {
+//     console.log("serh yazin");
+//   }
+// }
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// var swiper2 = new Swiper(".mySwiper2", {
+//   watchSlidesProgress: true,
+//   slidesPerView: 5,
+//   spaceBetween: 16,
+//   // loop: true,
+// });
+
+var swiper3 = new Swiper(".mySwiper3", {
+  watchSlidesProgress: true,
+  slidesPerView: 3,
+  spaceBetween: 16,
+  loop: true,
+});
