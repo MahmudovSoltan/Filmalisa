23d186109552566de7823d168275b687d7c8c332
Soltan <mahmudovsoltan477@gmail.com>
2025-01-17 16:15:56 +0400

    admin movies add remove btn

--- Admin/Js/Movies/movies.js
+++ Admin/Js/Movies/movies.js
@@ -1,15 +1,25 @@
 const creatBtn = document.querySelector("#creatBtn")
-const movieModal = document.querySelector("#movieModal")
+const movieModal2 = document.querySelector("#movieModal2")
 const owarlay = document.querySelector(".owarlay")
 const creat = document.querySelector("#creat")
-
+const table_delete_btn = document.querySelector(".table_delete_btn")
+const movieModal = document.querySelector("#movieModal")
+const owarlay2 = document.querySelector("#owarlay2")
+console.log(movieModal);
 
 creatBtn.addEventListener("click",()=>{
-    movieModal.classList.add('active');
+    movieModal2.classList.add('active');
 })
 owarlay.addEventListener("click",()=>{
-    movieModal.classList.remove('active');  
+    movieModal2.classList.remove('active');  
 })
 creat.addEventListener("click",()=>{
-    movieModal.classList.add('active');
+    movieModal2.classList.add('active');
+})
+
+table_delete_btn.addEventListener("click",()=>{
+    movieModal.classList.add("active") 
+})
+owarlay2.addEventListener("click",()=>{
+    movieModal.classList.remove("active") 
 })
