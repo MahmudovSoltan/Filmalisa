84744568ed46abc28a57b41739ba19972c78701a
RamilShahmerdanov <shahmerdanovr20@gmail.com>
2025-01-24 10:17:12 +0400

    change js

--- Client/Js/detailed.js
+++ Client/Js/detailed.js
@@ -1,16 +1,16 @@
-// function addComment() {
-//   const commentInput = document.querySelector("#commentInput");
-//   const addedcomment = document.querySelector("#addedcomment");
-//   data = commentInput.value.jSON;
+function addComment() {
+  const commentInput = document.querySelector("#commentInput");
+  const addedcomment = document.querySelector("#addedcomment");
+  data = commentInput.value.jSON;
 
-//   if (commentInput.value.trim() !== "") {
-//     addedcomment.innerText += commentInput.value;
-//     commentInput.value = "";
-//     console.log(commentInput.value);
-//   } else {
-//     console.log("serh yazin");
-//   }
-// }
+  if (commentInput.value.trim() !== "") {
+    addedcomment.innerText += commentInput.value;
+    commentInput.value = "";
+    console.log(commentInput.value);
+  } else {
+    console.log("serh yazin");
+  }
+}
 
 // var swiper = new Swiper(".mySwiper", {
 //   slidesPerView: 1,
