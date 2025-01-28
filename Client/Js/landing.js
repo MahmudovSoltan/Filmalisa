ad73d823d7a6cabba113c8ea49923b2e4b643a87
NarminJalilova <adilovanarmin95@gmail.com>
2025-01-28 14:19:47 +0400

    added API

--- Client/Js/landing.js
+++ Client/Js/landing.js
@@ -22,18 +22,59 @@ faqItems.forEach(item => {
     });
 });
 
+// Contactform APİ hissesi
+
+    // Formdan məlumatları alırıq
+    let fullName = document.querySelector("#nameInput");
+    let email = document.querySelector("#emailInput");
+    let reason = document.querySelector("#reasonInput");
+    let sendInfo = document.querySelector("#sendInfo");
+
+    // API-ə POST istəyi göndəririk
+    async function getContact() {
+        const formData = {
+            full_name: fullName.value,
+            email: email.value,
+            reason: reason.value
+        };
+        console.log(formData);
+        try {
+            const response = await fetch("https://api.sarkhanrahimli.dev/api/filmalisa/contact", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",               
+                  },
+                body: JSON.stringify(formData) // Form məlumatlarını göndəririk
+            });
+
+            // API-dən cavabı alırıq
+            const data = await response.json();
+
+            fullName.value = "";
+            email.value = "";
+            reason.value = "";
+
+            // Cavabda problem varsa
+            if (!data.result) {
+                sendInfo.innerHTML= "Please fill in all fields";
+                sendInfo.style.color = "#ff4d4d";          
+            }else {
+                sendInfo.innerHTML = "Your message has been sent successfully!";   
+                sendInfo.style.color = "#2e7d32";             
+            }
+
+            // Cavab uğurludursa
+            console.log('Server response:', data);
 
 
+            // API-dən alınan məlumatı göstəririk 
+            console.log('Received data:', data.data);
+            // Məsələn, serverdən gələn `full_name` və `email`-i göstərə bilərik:
+            alert(`Thank you ${data.data.full_name}, we have received your message!`);
 
-
-
-
-
-
-
-
-
-
-
+        } catch (error) {
+            console.log('Error:', error); 
+        }
+    }
 
 
